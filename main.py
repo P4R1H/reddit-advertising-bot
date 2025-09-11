ac2863717f2c9e0df1b2be106367ab6bfda0133a
Aman25ta <aman25.ta@gmail.com>
2025-09-11 09:59:55 +0300

    2025-09-11. swapped db to mongo;test on prod later

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ settings = json.load(f)
 f.close()
 subreddits = {}
 for setting in settings:
-  if setting == "channelid":
-    channel = settings[setting]
-  elif setting  == "dmcontent":
-    dmcontent = settings[setting]
-  elif setting  == "dmtitle":
-    dmtitle = settings[setting] 
-  else:
-    subreddits[setting] = settings[setting] 
+    if setting == "channelid":
+        channel = settings[setting]
+    elif setting  == "dmcontent":
+        dmcontent = settings[setting]
+    elif setting  == "dmtitle":
+        dmtitle = settings[setting] 
+    else:
+        subreddits[setting] = settings[setting] 
 
  
 
@@ -38,29 +38,29 @@ async def advertise():
     ctx = client.get_channel(channel)
     while(1):
 
-      await ctx.send("```Hot invite batch incoming```")
-      for sub in subreddits:
-        await advertisehot(ctx, sub, subreddits[sub], dmtitle, dmcontent)      
-      await ctx.send("```waiting 1 hour to send next batch of invites```")
-      await asyncio.sleep(3600)
+        await ctx.send("```Hot invite batch incoming```")
+        for sub in subreddits:
+            await advertisehot(ctx, sub, subreddits[sub], dmtitle, dmcontent)      
+        await ctx.send("```waiting 1 hour to send next batch of invites```")
+        await asyncio.sleep(3600)
 
 
 
-      await ctx.send("```new invite batch incoming```")
-      for sub in subreddits:
-        await advertisenew(ctx, sub, 15, dmtitle, dmcontent)
-      await ctx.send("```waiting 1 hour to send next batch of invites```")
-      await asyncio.sleep(3600)
+        await ctx.send("```new invite batch incoming```")
+        for sub in subreddits:
+            await advertisenew(ctx, sub, 15, dmtitle, dmcontent)
+        await ctx.send("```waiting 1 hour to send next batch of invites```")
+        await asyncio.sleep(3600)
 
 
 advertise.start()
 @advertise.before_loop
 async def before_advertise():
-  await client.wait_until_ready()
+    await client.wait_until_ready()
 
 
 
 
 #running bot
-keep_alive()
+keep_alive() # remove later unless using uptimerbot
 client.run(os.getenv("token"))
