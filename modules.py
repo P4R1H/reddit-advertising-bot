8f17d6e6746e89ae4261eb43f8f08dc8917807dc
Aman25ta <aman25.ta@gmail.com>
2025-09-18 12:57:55 +0200

    added comments and ignore list.

--- modules.py
+++ modules.py
@@ -25,6 +25,8 @@ async def login():
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='-', intents=intents)
 
+antidmlist = os.getenv("antidm").split(",")
+
 #modules
 async def advertisehot(subreddit, lim, title, content):
     await logger.log_info("_ _")
@@ -38,35 +40,83 @@ async def advertisehot(subreddit, lim, title, content):
         await logger.log_info(f"starting {subreddit}")
         
         try:
-            dbuser = await mdb.user_find(submission.author.name.lower())
-            if dbuser and os.getenv("current_camp") in dbuser["camps"]:
-                await logger.log_info(submission.author.name +
-                      ": found in the dm log, going to next user")
-                continue
+            if submission.author.name.lower() in (s.lower() for s in antidmlist):
+                await logger.log_info(f"{submission.author.name} in antidm list. ignored.")
+                pass
             else:
-                await logger.log_info(submission.author.name + ": didnt find in the dm log")
+                dbuser = await mdb.user_find(submission.author.name.lower())
+                if dbuser and os.getenv("current_camp") in dbuser["camps"]:
+                    await logger.log_info(submission.author.name +
+                        ": found in the dm log, going to next user")
+                else:
+                    await logger.log_info(submission.author.name + ": didnt find in the dm log")
 
 
-                # add user here
-                # would use update_user here since we are checking camps.
-                await mdb.update_user(submission.author.name.lower())
+                    # add user here
+                    # would use update_user here since we are checking camps.
+                    await mdb.update_user(submission.author.name.lower())
 
-                await logger.log_info("written in the dm log")
+                    await logger.log_info("written in the dm log")
 
-                tuser = await reddit.redditor(submission.author.name)
+                    tuser = await reddit.redditor(submission.author.name)
 
-                await tuser.message(title,content)
+                    await tuser.message(title,content)
 
-                await logger.log_info(
-                    f"{submission.author.name} has been dmd an invite")                          
-                await asyncio.sleep(45)
+                    await logger.log_info(
+                        f"{submission.author.name} has been dmd an invite")                          
+                    await asyncio.sleep(45)
           
         except Exception as e:
             await logger.log_warning(e)
             await logger.log_info(f"{submission.author.name} probably has dms off, not added to dm log")
             await logger.log_warning(f"Couldnt dm {submission.author.name}, they probably have dms off")
-            continue
 
+        # go thru comments also now
+        try:
+            await submission.load()
+            await submission.comments.replace_more(limit=0)
+            comments = sorted(
+                submission.comments.list(),
+                key=lambda c: c.score if c.author else 0,
+                reverse=True
+            )[:15] # only top 15 here, inc/dec as needed.
+
+            for comment in comments:
+                if not comment.author:
+                    continue  # skip deleted users
+                if comment.author.name.lower() in (s.lower() for s in antidmlist):
+                    await logger.log_info(f"{comment.author.name} in antidm list. ignored.")
+                    pass
+                try:
+                    dbuser = await mdb.user_find(comment.author.name.lower())
+                    if dbuser and os.getenv("current_camp") in dbuser["camps"]:
+                        await logger.log_info(comment.author.name +
+                            ": found in the dm log, skipping")
+                        continue
+
+                    await logger.log_info(comment.author.name + ": didnt find in the dm log")
+
+                    await mdb.update_user(comment.author.name.lower())
+                    await logger.log_info("written in the dm log")
+
+                    tuser = await reddit.redditor(comment.author.name)
+                    await tuser.message(title, content)
+
+                    await logger.log_info(
+                        f"{comment.author.name} has been dmd an invite")
+                    await asyncio.sleep(45)
+
+                except Exception as ce:
+                    await logger.log_warning(ce)
+                    await logger.log_info(
+                        f"{comment.author.name} probably has dms off, not added to dm log")
+                    await logger.log_warning(
+                        f"Couldnt dm {comment.author.name}, they probably have dms off")
+
+        except Exception as e:
+            await logger.log_warning(e)
+            await logger.log_warning(
+                f"Couldnt process comments for {submission.id}")
 
 
 
@@ -81,29 +131,32 @@ async def advertisenew(subreddit, lim, title, content):
 
     async for submission in bottomsub:
         await logger.log_info(f"starting {subreddit}")
-        
-        try:
-            dbuser = await mdb.user_find(submission.author.name.lower())
-            if dbuser and os.getenv("current_camp") in dbuser["camps"]:
-                await logger.log_info(submission.author.name +
-                      ": found in the dm log, going to next user")
-                continue
-            else:
-                await logger.log_info(submission.author.name + ": didnt find in the dm log")
+        if submission.author.name.lower() in (s.lower() for s in antidmlist):
+            await logger.log_info(f"{submission.author.name} in antidm list. ignored.")
+            continue
+        else:
+            try:
+                dbuser = await mdb.user_find(submission.author.name.lower())
+                if dbuser and os.getenv("current_camp") in dbuser["camps"]:
+                    await logger.log_info(submission.author.name +
+                        ": found in the dm log, going to next user")
+                    continue
+                else:
+                    await logger.log_info(submission.author.name + ": didnt find in the dm log")
 
-                await mdb.update_user(submission.author.name.lower())
-                
-                await logger.log_info("written in the dm log")
+                    await mdb.update_user(submission.author.name.lower())
+                    
+                    await logger.log_info("written in the dm log")
 
-                tuser = await reddit.redditor(submission.author.name)
+                    tuser = await reddit.redditor(submission.author.name)
 
-                await tuser.message(title, content)
+                    await tuser.message(title, content)
 
-                await logger.log_info(
-                    f"{submission.author.name} has been dmd an invite")                          
-                await asyncio.sleep(45)
-          
-        except:
-            await logger.log_info(f"{submission.author.name} probably has dms off, not added to dm log")
-            await logger.log_warning(f"Couldnt dm {submission.author.name}, they probably have dms off")
-            continue          
+                    await logger.log_info(
+                        f"{submission.author.name} has been dmd an invite")                          
+                    await asyncio.sleep(45)
+            
+            except:
+                await logger.log_info(f"{submission.author.name} probably has dms off, not added to dm log")
+                await logger.log_warning(f"Couldnt dm {submission.author.name}, they probably have dms off")
+                continue          
